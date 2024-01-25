25ac9a18a172c739043eb6a7156b6a71e93bf62d
Jin <jinnkimm7@gmail.com>
2024-01-25 16:49:20 +0900

    Fix: 인코딩 필터 수정

--- mvc/src/main/java/com/nhnacademy/student/filter/CharacterEncodingFilter.java
+++ mvc/src/main/java/com/nhnacademy/student/filter/CharacterEncodingFilter.java
@@ -1,15 +1,19 @@
 package com.nhnacademy.student.filter;
 
+import lombok.extern.slf4j.Slf4j;
+
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
 import java.io.IOException;
 
+@Slf4j
 @WebFilter("/*")
 public class CharacterEncodingFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request,
                          ServletResponse response,
                          FilterChain chain) throws IOException, ServletException {
+        request.setCharacterEncoding("utf-8");
         response.setCharacterEncoding("utf-8");
         chain.doFilter(request, response);
     }
