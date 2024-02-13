def007f086263cd499456f2f135ea9ace345b376
Jin <jinnkimm7@gmail.com>
2024-02-13 11:13:08 +0900

    init : spring core
    maven
    1. spring-context 라이브러리 의존성을 추가
    2. spring-framework-bom 사용 실습

---
+++ springframework-core-message-sender/src/main/java/com/nhnacademy/edu/springframework/Main.java
@@ -0,0 +1,9 @@
+package com.nhnacademy.edu.springframework;
+
+import com.nhnacademy.edu.springframework.message.User;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello, world!");
+    }
+}
