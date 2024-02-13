16e01642cfbd88a0bf17f007250fb027d794ee91
Jin <jinnkimm7@gmail.com>
2024-02-13 14:47:40 +0900

    스프링 빈 스코프 설정

--- springframework-core-message-sender/src/main/java/com/nhnacademy/edu/springframework/message/XmlMain.java
+++ springframework-core-message-sender/src/main/java/com/nhnacademy/edu/springframework/message/XmlMain.java
@@ -8,6 +8,9 @@ public class XmlMain {
             MessageSender smsMessageSender = context.getBean("smsMessageSender", MessageSender.class);
             MessageSender emailMessageSender = context.getBean("emailMessageSender", MessageSender.class);
 
+            MessageSender smsMessageSender1 = context.getBean("smsMessageSender", MessageSender.class);
+            MessageSender emailMessageSender1 = context.getBean("emailMessageSender", MessageSender.class);
+
             User user = new User("kim@nhnacademy.com", "010-XXXX-XXXX");
             MessageSendService messageSendService1 = new MessageSendService(smsMessageSender);
             messageSendService1.doSendMessage(user, "hello");
