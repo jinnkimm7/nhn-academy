5874c61ab95172b776acba56ec3f374eb18ae91d
Jin <jinnkimm7@gmail.com>
2024-02-13 14:19:38 +0900

    스프링 빈

---
+++ springframework-core-message-sender/src/main/java/com/nhnacademy/edu/springframework/message/XmlMain.java
@@ -0,0 +1,19 @@
+package com.nhnacademy.edu.springframework.message;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class XmlMain {
+    public static void main(String[] args) {
+        try (ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("beans.xml")) {
+            MessageSender smsMessageSender = context.getBean("smsMessageSender", MessageSender.class);
+            MessageSender emailMessageSender = context.getBean("emailMessageSender", MessageSender.class);
+
+            User user = new User("kim@nhnacademy.com", "010-XXXX-XXXX");
+            MessageSendService messageSendService1 = new MessageSendService(smsMessageSender);
+            messageSendService1.doSendMessage(user, "hello");
+
+            MessageSendService messageSendService2 = new MessageSendService(emailMessageSender);
+            messageSendService2.doSendMessage(user, "hello");
+        }
+    }
+}
