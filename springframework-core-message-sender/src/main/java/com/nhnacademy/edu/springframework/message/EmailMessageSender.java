ae9d7e8e5211fe52250b7c01fd7fae4cb5d0a6c9
Jin <jinnkimm7@gmail.com>
2024-02-13 15:17:50 +0900

    Spring IoC 실습 - init-method, destroy-method

--- springframework-core-message-sender/src/main/java/com/nhnacademy/edu/springframework/message/EmailMessageSender.java
+++ springframework-core-message-sender/src/main/java/com/nhnacademy/edu/springframework/message/EmailMessageSender.java
@@ -1,8 +1,18 @@
 package com.nhnacademy.edu.springframework.message;
 
-public class EmailMessageSender implements MessageSender{
+import org.springframework.beans.factory.InitializingBean;
+
+public class EmailMessageSender implements MessageSender {
     @Override
     public void sendMessage(User user, String message) {
         System.out.println(message + " from " + user.getEmail());
+    }
+
+    public void init() {
+        System.out.println("EmailMessageSender initialized!!");
+    }
+
+    public void cleanup() {
+        System.out.println("EmailMessageSender cleanup called!!");
     }
 }
--- springframework-core-message-sender/src/main/java/com/nhnacademy/edu/springframework/message/SmsMessageSender.java
+++ springframework-core-message-sender/src/main/java/com/nhnacademy/edu/springframework/message/SmsMessageSender.java
@@ -1,8 +1,17 @@
 package com.nhnacademy.edu.springframework.message;
 
-public class SmsMessageSender implements MessageSender{
+import org.springframework.beans.factory.InitializingBean;
+
+public class SmsMessageSender implements MessageSender {
     @Override
     public void sendMessage(User user, String message) {
         System.out.println(message + " from " + user.getPhoneNumber());
+    }
+
+    public void init() {
+        System.out.println("SmsMessageSender initialized!!");
+    }
+    public void cleanup() {
+        System.out.println("SmsMessageSender cleanup called!!");
     }
 }
