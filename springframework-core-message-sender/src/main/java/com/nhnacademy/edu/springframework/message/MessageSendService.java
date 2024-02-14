e3eac4e3038c24405e6be60f520bf8600c61d24c
Jin <jinnkimm7@gmail.com>
2024-02-14 16:07:08 +0900

    Spring DI 실습 - annotation

--- springframework-core-message-sender/src/main/java/com/nhnacademy/edu/springframework/message/MessageSendService.java
+++ springframework-core-message-sender/src/main/java/com/nhnacademy/edu/springframework/message/MessageSendService.java
@@ -1,19 +1,27 @@
 package com.nhnacademy.edu.springframework.message;
 
-public class MessageSendService {
-    private MessageSender messageSender;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 
-//    public MessageSendService(MessageSender messageSender) {
+import java.util.List;
+
+public class MessageSendService {
+    // 같은 타입의 객체가 여러개 존재하는 경우, 배열이나 Collection 으로 의존성을 주입할 수 있습니다.
+    private final List<MessageSender> messageSenders;
+
+//    스프링 프레임워크에서는 생성자 방식으로 의존성 주입하는 것을 권장합니다.
+    @Autowired
+    public MessageSendService(@Qualifier("emailMessageSender") List<MessageSender> messageSenders) {
+        this.messageSenders = messageSenders;
+    }
+
+//    setter 메소드가 없더라도 field에 직접 @Autowired를 설정하여 의존성을 주입할 수 있습니다.
+//    public void setSmsMessageSender(MessageSender messageSender) {
+//        System.out.println("setMessageSender invoked!!");
 //        this.messageSender = messageSender;
 //    }
 
-    public MessageSendService() {}
-    public void setSmsMessageSender(MessageSender messageSender) {
-        System.out.println("setMessageSender invoked!!");
-        this.messageSender = messageSender;
-    }
-
     public void doSendMessage(User user, String message) {
-        messageSender.sendMessage(user, message);
+        messageSenders.forEach(messageSender -> messageSender.sendMessage(user, message));
     }
 }
