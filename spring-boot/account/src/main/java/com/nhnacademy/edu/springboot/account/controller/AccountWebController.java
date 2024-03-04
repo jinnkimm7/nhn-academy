c3e1b461f2ecc5a8c530155f54c74072fbb17225
Jin <jinnkimm7@gmail.com>
2024-03-04 15:40:06 +0900

    Spring-Boot의 View (Thymeleaf)

---
+++ spring-boot/account/src/main/java/com/nhnacademy/edu/springboot/account/controller/AccountWebController.java
@@ -0,0 +1,19 @@
+package com.nhnacademy.edu.springboot.account.controller;
+
+import com.nhnacademy.edu.springboot.account.domain.Account;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class AccountWebController {
+    @GetMapping("/web/accounts/{id}")
+    public  String getAccount(
+            @PathVariable Long id,
+            Model model
+    ) {
+        model.addAttribute("account", new Account("1", 10000));
+        return "account";
+    }
+}
