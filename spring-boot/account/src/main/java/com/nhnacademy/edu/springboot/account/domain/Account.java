0b101355b1b9c2ba5582d320c90249b7018b8f9c
Jin <jinnkimm7@gmail.com>
2024-03-04 13:18:07 +0900

    Spring Boot로 JPA 적용 실습

--- spring-boot/account/src/main/java/com/nhnacademy/edu/springboot/account/domain/Account.java
+++ spring-boot/account/src/main/java/com/nhnacademy/edu/springboot/account/domain/Account.java
@@ -2,13 +2,22 @@ package com.nhnacademy.edu.springboot.account.domain;
 
 import lombok.*;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
 @Getter
 @Setter
 @EqualsAndHashCode
-@ToString
+@Entity
 public class Account {
+    @Id @GeneratedValue
+    private Long id;
     private String number;
     private Integer balance;
+
+    public Account() {
+    }
 
     public Account(String number, Integer balance) {
         this.number = number;
--- spring-boot/account/src/test/java/com/nhnacademy/edu/springboot/account/service/DefaultAccountServiceTest.java
+++ spring-boot/account/src/test/java/com/nhnacademy/edu/springboot/account/service/DefaultAccountServiceTest.java
@@ -2,21 +2,29 @@ package com.nhnacademy.edu.springboot.account.service;
 
 import com.nhnacademy.edu.springboot.account.domain.Account;
 import com.nhnacademy.edu.springboot.account.repository.AccountRepository;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
 class DefaultAccountServiceTest {
     @Autowired
-    private AccountService accountService;
+    private AccountRepository accountRepository;
+
     @Test
     void testGetAccounts() {
-        List<Account> actual = accountService.getAccounts();
-        assertThat(actual).hasSize(2);
+        Account account1 = new Account("1", 10000);
+        accountRepository.save(account1);
+
+        Optional<Account> actual = accountRepository.findById(1L);
+
+        Assertions.assertThat(actual).isPresent();
+        Assertions.assertThat(actual.get()).isEqualTo(account1);
     }
 }
